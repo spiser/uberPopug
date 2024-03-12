368fcced9778b8669f44c172bea3935c71bd7365
s.spicin <s.spicin@kt-team.de>
2024-03-13 02:19:05 +0400

    upgrade analytics service

--- analytics-service/routes/web.php
+++ analytics-service/routes/web.php
@@ -1,7 +1,45 @@
 <?php
 
+use App\Http\Controllers\Auth\SSOController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::middleware(['auth', 'verified'])
+    ->get('/dashboard', [DashboardController::class, 'view'])->name('dashboard');
+
+
+Route::get('/sso/login', [SSOController::class, 'login'])
+    ->name('sso.login');
+
+Route::get('/sso/callback',[SSOController::class, 'callback'])
+    ->name('sso.callback');
+
+Route::get('/sso/user-info',[SSOController::class, 'userInfo'])
+    ->name('sso.userInfo');
+
+
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
